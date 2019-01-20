581659c0e2c959795e3596084f5ac3b470aea53b
yerin <hepari2154@naver.com>
2019-01-20 17:47:20 +0900

    Fix transaction 추가

--- src/main/java/com/example/starbucksorder/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/starbucksorder/service/impl/ProductServiceImpl.java
@@ -8,11 +8,13 @@ import com.example.starbucksorder.repository.ProductRepository;
 import com.example.starbucksorder.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 
 @Service
+@Transactional
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
